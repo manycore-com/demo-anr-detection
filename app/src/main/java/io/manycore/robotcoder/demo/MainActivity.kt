257221a835a84b276d6850caf4e6a540b1dfdafc
nicotop <ntoper@gmail.com>
2022-07-05 21:15:04 -0700

    first init

--- app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
+++ app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
@@ -1,11 +1,19 @@
 package io.manycore.robotcoder.demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Copy paste from Glide repo on https://github.com/bumptech/glide
+        val imageView: ImageView = findViewById<View>(R.id.my_image_view) as ImageView
+        Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView)
     }
 }
