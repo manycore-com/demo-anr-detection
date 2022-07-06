709bff401064f67fe9e62b2240f1d46aecbe7913
nicotop <ntoper@gmail.com>
2022-07-05 19:19:04 -0700

    first example

--- app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
+++ app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
@@ -10,8 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val storageDirectory =
-            getExternalFilesDir("your_path")?.absolutePath
+        val storageDirectory = getExternalFilesDir("your_path")?.absolutePath
 
 
 
