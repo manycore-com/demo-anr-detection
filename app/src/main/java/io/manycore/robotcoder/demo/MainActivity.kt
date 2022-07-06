d10752089d88e037f4ae93dde2db740f447338a0
nicotop <ntoper@gmail.com>
2022-07-05 21:06:27 -0700

    first init

--- app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
+++ app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
@@ -1,11 +1,29 @@
 package io.manycore.robotcoder.demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mp:MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mp = MediaPlayer.create(this, R.raw.clip)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        super.onResume()
+        mp.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mp.stop()
+    }
+
 }
