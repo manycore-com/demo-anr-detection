0237dba23185b11fd8773671d1f51cb84140f8e7
nicotop <ntoper@gmail.com>
2022-07-07 10:37:49 -0700

    calling init

--- app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
+++ app/src/main/java/io/manycore/robotcoder/demo/MainActivity.kt
@@ -2,10 +2,12 @@ package io.manycore.robotcoder.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.manycore.robotcoder.demo.utils.Init
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val i = Init(baseContext)
     }
 }
