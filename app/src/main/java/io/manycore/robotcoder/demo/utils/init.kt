629de4012521b2335237018d738c3cf56b434374
nicotop <ntoper@gmail.com>
2022-07-07 10:37:09 -0700

    add a new class

---
+++ app/src/main/java/io/manycore/robotcoder/demo/utils/init.kt
@@ -0,0 +1,12 @@
+package io.manycore.robotcoder.demo.utils
+
+import android.content.Context
+
+class Init(ctx: Context) {
+
+    val extPath = ctx.getExternalFilesDir(null).absolutePath
+
+    fun makeThings() {
+        //do something here
+    }
+}
